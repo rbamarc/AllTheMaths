94877f058cf6bfff14a483d968cc99c14518a1a4
rbamarc <rbamarc929@gmail.com>
2023-01-29 21:12:38 -0800

    first commit

---
+++ src/main/java/netbeans/allthemaths/AllTheMaths.java
@@ -0,0 +1,24 @@
+package netbeans.allthemaths;
+
+/**
+ *
+ * @author halfrikan
+ */
+public class AllTheMaths {
+
+    public static void main(String[] args) {
+        System.out.print("1 + 2 is: ");
+        System.out.println(1 + 2);
+        
+        System.out.print("42001 modulus 5 is: ");
+        System.out.println(42001 % 5);
+        
+        System.out.print("5565.0 divided by 22.0 is: ");
+        System.out.println(5565.0 / 22.0);
+        
+        System.out.print("233 times 31 minus 42 is: ");
+        System.out.println(233 * 31 - 42);
+        
+        System.out.print("");
+    }
+}
