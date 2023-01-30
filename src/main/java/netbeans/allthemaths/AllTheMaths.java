1cb0990836b7aae5bfa653fb43e008c8aaf80a31
rbamarc <rbamarc929@gmail.com>
2023-01-29 22:02:07 -0800

    added more math

--- src/main/java/netbeans/allthemaths/AllTheMaths.java
+++ src/main/java/netbeans/allthemaths/AllTheMaths.java
@@ -19,6 +19,15 @@ public class AllTheMaths {
         System.out.print("233 times 31 minus 42 is: ");
         System.out.println(233 * 31 - 42);
         
-        System.out.print("");
+        System.out.print("Is 4 greater than -1?");
+        System.out.println(4 > -1);
+        
+        System.out.println("\n****** Now make the computer do some harder math!");
+        
+        System.out.print("8043.52 minus 4.2 plus 23.0 divided by 56.0 times -76.13 is: ");
+        System.out.println(8043.52 - 4.2 + 23.0 / 56.0 * -76.13);
+        
+        System.out.print("11111 modulus 3 minus 67 minus 1 plus 9 is: ");
+        System.out.println(11111 % 3 - 67 - 1 + 9);
     }
 }
